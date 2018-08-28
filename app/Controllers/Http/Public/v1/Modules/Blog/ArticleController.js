6e1c0ef2cf80e7c09d4eb27a577f15adc65065f5
Ralph Huwiler <ralph@huwiler.rocks>
2018-08-28 14:48:15 +0200

    reverse order events in the future

--- app/Controllers/Http/Public/v1/Modules/Blog/ArticleController.js
+++ app/Controllers/Http/Public/v1/Modules/Blog/ArticleController.js
@@ -14,15 +14,15 @@ class ArticleController {
       .fetch()
 
     // Custom order for event dates
-    articles = _.orderBy(articles.rows, article => {
+    articles = _.orderBy(articles.rows, [article => {
       // If there is an event date, sort by this
       if (article.event_at) {
-        return article.event_at
+        return moment(article.event_at).isAfter() ? moment() : article.event_at
       }
 
       // otherwise sort by publication date
       return article.published_at
-    }, 'desc')
+    }, article => article.event_at], ['desc', 'asc'])
 
     return transform.collection(articles, ArticleTransformer)
   }
